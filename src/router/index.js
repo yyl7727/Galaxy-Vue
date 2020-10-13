be5d89b87c9e8f14c6a3255de4b06b4d14084285
yyl7727 <772788236@qq.com>
2020-10-13 10:12:03 +0800

    登录页

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
 import Index from '../views/Index.vue'
-import BlogEdit from '../views/BlogEdit.vue'
-import BlogDetail from '../views/BlogDetail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Login',
-    component: Login
-  },
-  {
     path: '/index',
     name: 'Index',
     component: Index
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/blog/add',
-    name: 'BlogEdit',
-    component: BlogEdit
-  },
-  {
-    path: '/blog/:blogId',
-    name: 'BlogDetail',
-    component: BlogDetail
-  },
-  {
-    path: '/blog/:blogId/edit',
-    name: 'BlogEdit',
-    component: BlogEdit
   }
 ]
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,62 @@
 <template>
-    <div>
-        index
+  <div id="login">
+    <div class="login-warp">
+      <ul class="menu-tab">
+        <li v-for="menuTab in menuTabs" :key="menuTab.id" :class="{'current': menuTab.current}" @click="toggleMenu(item)">{{menuTab.text}}</li>
+        <li></li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Index.vue"
-}
+  name: "login",
+  data() {
+    return {
+      menuTabs: [
+        { text: '登录', current: true },
+        { text: '注册', current: false }
+      ],
+      isActive: true
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      console.log(data)
+    }
+  },
+};
 </script>
 
-<style>
+<style scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
 
+.login-warp {
+  width: 330px;
+  margin: auto;
+}
+
+.menu-tab {
+  text-align: center;
+}
+
+.menu-tab li {
+  display: inline-block;
+  width: 88px;
+  line-height: 36px;
+  font-size: 14px;
+  color: #fff;
+  border-radius: 2px;
+  cursor: pointer;
+}
+
+.current {
+  background-color: rgba(0, 0, 0, .1);
+}
 </style>
